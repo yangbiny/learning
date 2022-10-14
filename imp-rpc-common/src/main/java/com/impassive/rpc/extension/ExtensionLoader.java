5a4e90787746630a471190e08fc0a79147fda9a8
impassive <impassive@aliyun.com>
2022-10-14 18:05:00 +0800

    fix

---
+++ imp-rpc-common/src/main/java/com/impassive/rpc/extension/ExtensionLoader.java
@@ -0,0 +1,14 @@
+package com.impassive.rpc.extension;
+
+/**
+ * 扩展点加载器
+ */
+public class ExtensionLoader {
+
+  public static <T> T buildExtension(Class<T> extensionType) {
+
+    return null;
+  }
+
+
+}
