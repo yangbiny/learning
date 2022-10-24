f033a8571bd5fef6368e36f7953db89426154f26
impassive <impassive@aliyun.com>
2022-10-24 13:59:28 +0800

    add netty

--- imp-rpc-remote/src/main/java/com/impassive/rpc/remote/netty/NettyRemoteService.java
+++ imp-rpc-remote/src/main/java/com/impassive/rpc/remote/netty/NettyRemoteService.java
@@ -2,11 +2,17 @@ package com.impassive.rpc.remote.netty;
 
 import com.impassive.rpc.common.URL;
 import com.impassive.rpc.remote.api.RemoteService;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.nio.NioEventLoopGroup;
 
 public class NettyRemoteService implements RemoteService {
 
   @Override
   public void openService(URL<?> url) {
-
+    ServerBootstrap serverBootstrap = new ServerBootstrap();
+    NioEventLoopGroup parentGroup = new NioEventLoopGroup();
+    NioEventLoopGroup childGroup = new NioEventLoopGroup();
+    serverBootstrap.group(parentGroup, childGroup)
+        .bind(url.getProtocol().port());
   }
 }
