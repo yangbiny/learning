4ab65ef627b8dda5e22658835fab42b50117a89a
impassive <impassive@aliyun.com>
2022-11-02 18:55:11 +0800

    fix

--- imp-rpc-core/src/main/java/com/impassive/rpc/core/api/Registry.java
+++ imp-rpc-core/src/main/java/com/impassive/rpc/core/api/Registry.java
@@ -4,8 +4,18 @@ import com.impassive.rpc.common.ImpUrl;
 
 public interface Registry {
 
+  /**
+   * 将 url 的信息，写入到注册中心
+   *
+   * @param impUrl url
+   */
   void register(ImpUrl<?> impUrl);
 
+  /**
+   * 从 注册中心 删除 当前url的信息。如果是 多个服务器，只会删除当前的IP
+   *
+   * @param impUrl url
+   */
   void unRegister(ImpUrl<?> impUrl);
 
 }
