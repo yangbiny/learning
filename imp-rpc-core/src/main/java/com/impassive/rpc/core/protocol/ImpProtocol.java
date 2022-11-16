1400701888581c48c48ee5c4adfdb441e49f86fa
impassive <impassive@aliyun.com>
2022-11-16 18:34:28 +0800

    fix

--- imp-rpc-core/src/main/java/com/impassive/rpc/core/protocol/ImpProtocol.java
+++ imp-rpc-core/src/main/java/com/impassive/rpc/core/protocol/ImpProtocol.java
@@ -3,13 +3,27 @@ package com.impassive.rpc.core.protocol;
 import com.impassive.rpc.common.ImpUrl;
 import com.impassive.rpc.core.api.Invoker;
 import com.impassive.rpc.core.api.Protocol;
+import com.impassive.rpc.exception.ExceptionCode;
+import com.impassive.rpc.exception.ServiceException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ImpProtocol implements Protocol {
+
+  private static final Map<Class<?>, Invoker<?>> INVOKER_MAP = new ConcurrentHashMap<>();
 
 
   @Override
   public void export(Invoker<?> invoker) {
+    Class<?> anInterface = invoker.getInterface();
+    Invoker<?> exist = INVOKER_MAP.get(anInterface);
+    if (exist != null) {
+      throw new ServiceException(ExceptionCode.SERVICE_EXPORTER_EXCEPTION, "该服务已经注册");
+    }
 
+
+
+    INVOKER_MAP.put(anInterface, invoker);
   }
 
   @Override
