e12cc8ea1356de3373a23cca75678629d8688b21
impassive <tingfa.yang@duitang.com>
2022-11-30 11:03:56 +0800

    add generics class

---
+++ imp-kotlin/src/main/kotlin/com/impassive/kotlin/basic/GenericsClass.kt
@@ -0,0 +1,27 @@
+package com.impassive.kotlin.basic
+
+/**
+ * @author impassive
+ */
+// 定义 范型类
+class GenericsClass<T> {
+
+    // 定义一个可变的List。  直接 是 List 是不可变的
+    private var value: MutableList<T> = mutableListOf()
+
+    fun add(args: T): Boolean {
+        return value.add(args)
+    }
+
+
+}
+
+fun main() {
+
+    // 需要指定 范型类型。如果有主构造函数，可以推断，则无需指定
+    val testClass = GenericsClass<String>()
+
+    // 也可以这样指定 范型类型。后面构造 函数 处的范型 可写 可忽略
+    val testClass2: GenericsClass<String> = GenericsClass();
+
+}
