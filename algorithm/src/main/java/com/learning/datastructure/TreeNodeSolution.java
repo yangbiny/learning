98fb7f7341c2153cde880484eede6e9246fc6b2c
tingfa.yang <tingfa.yang@duitang.com>
2023-11-24 16:53:24 +0800

    add es8 query

--- algorithm/src/main/java/com/learning/datastructure/TreeNodeSolution.java
+++ algorithm/src/main/java/com/learning/datastructure/TreeNodeSolution.java
@@ -4,12 +4,57 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Queue;
+import java.util.Stack;
 import java.util.concurrent.LinkedTransferQueue;
 
 /**
  * @author impassive
  */
 public class TreeNodeSolution {
+
+
+  public List<Integer> preorderTraversal2(TreeNode root) {
+    ArrayList<Integer> result = new ArrayList<>();
+    if (root == null) {
+      return result;
+    }
+    Stack<TreeNode> stack = new Stack<>();
+    stack.push(root);
+
+    while (!stack.isEmpty()) {
+      TreeNode pop = stack.pop();
+      result.add(pop.val);
+      if (pop.right != null) {
+        stack.push(pop.right);
+      }
+      if (pop.left != null) {
+        stack.push(pop.left);
+      }
+    }
+
+    return result;
+  }
+
+
+  public List<Integer> preorderTraversal(TreeNode root) {
+    if (root == null) {
+      return null;
+    }
+
+    List<Integer> result = new ArrayList<>();
+    preorder(root.right, result);
+    return result;
+  }
+
+  private void preorder(TreeNode root, List<Integer> result) {
+    if (root == null) {
+      return;
+    }
+    result.add(root.val);
+
+    preorder(root.left, result);
+    preorder(root.right, result);
+  }
 
 
   public List<List<Integer>> levelOrder(TreeNode root) {
