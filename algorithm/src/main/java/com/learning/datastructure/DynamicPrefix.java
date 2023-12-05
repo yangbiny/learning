48c50ef6af024f58c9a98e73c910d80bc2ef8915
tingfa.yang <tingfa.yang@duitang.com>
2023-12-05 18:32:15 +0800

    fix

--- algorithm/src/main/java/com/learning/datastructure/DynamicPrefix.java
+++ algorithm/src/main/java/com/learning/datastructure/DynamicPrefix.java
@@ -1,5 +1,8 @@
 package com.learning.datastructure;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * @author impassive
  */
@@ -8,6 +11,49 @@ public class DynamicPrefix {
   public static void main(String[] args) {
     String[] strs = {"ab", "a"};
     System.out.println(new DynamicPrefix().longestCommonPrefix(strs));
+
+    int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
+    int[] x = new DynamicPrefix().lengthOfLIS2(nums);
+    System.out.println(x);
+  }
+
+  public int[] lengthOfLIS2(int[] nums) {
+    int[] dp = new int[nums.length];
+
+    Arrays.fill(dp, 1);
+    int[] result = new int[nums.length];
+    for (int i = 0; i < result.length; i++) {
+      result[i] = i;
+    }
+
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = 0; j < i; j++) {
+        if (nums[i] > nums[j]) {
+          if (dp[i] > dp[j] + 1) {
+            result[i] = i;
+          } else {
+            dp[i] = dp[j] + 1;
+            result[i] = j;
+          }
+        }
+      }
+    }
+
+    int max = dp[0];
+    int maxIndex = 0;
+    for (int i = 0; i < dp.length; i++) {
+      if (dp[i] > max) {
+        max = dp[i];
+        maxIndex = i;
+      }
+    }
+    int[] temp = new int[max];
+    for (int i = max - 1; i >= 0; i--) {
+      temp[i] = nums[maxIndex];
+      maxIndex = result[maxIndex];
+    }
+
+    return temp;
   }
 
   public String longestCommonPrefix(String[] strs) {
