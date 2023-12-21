a976d8d40bcdddcba079fb20ea90793519c742bc
tingfa.yang <tingfa.yang@duitang.com>
2023-12-21 18:00:11 +0800

    fix

--- algorithm/src/main/java/com/learning/datastructure/DynamicPrefix.java
+++ algorithm/src/main/java/com/learning/datastructure/DynamicPrefix.java
@@ -10,6 +10,17 @@ public class DynamicPrefix {
 
   public static void main(String[] args) {
 
+    int[][] minPathSum = {
+        {1, 3, 1},
+        {1, 5, 1},
+        {1, 4, 1}
+    };
+    int i4 = new DynamicPrefix().minPathSum(minPathSum);
+    System.out.println(i4);
+
+    int i3 = new DynamicPrefix().climbStairs(10);
+    System.out.println(i3);
+
     int i2 = new DynamicPrefix().longestCommonSubsequence("abcde", "ace");
     System.out.println(i2);
 
@@ -38,6 +49,40 @@ public class DynamicPrefix {
     int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
     int[] x = new DynamicPrefix().lengthOfLIS2(nums);
     System.out.println(x);
+  }
+
+  public int minPathSum(int[][] grid) {
+    int[][] dp = new int[grid.length][grid[0].length];
+    dp[0][0] = grid[0][0];
+    for (int i = 1; i < grid[0].length; i++) {
+      dp[0][i] = dp[0][i - 1] + grid[0][i];
+    }
+
+    for (int i = 1; i < grid.length; i++) {
+      for (int j = 0; j < grid[i].length; j++) {
+        if (j > 0) {
+          dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+        } else {
+          dp[i][j] = dp[i - 1][j] + grid[i][j];
+        }
+
+      }
+    }
+
+    return dp[grid.length - 1][grid[0].length - 1];
+  }
+
+  public int climbStairs(int n) {
+    if (n == 1 || n == 2) {
+      return n;
+    }
+    int[] dp = new int[n + 1];
+    dp[1] = 1;
+    dp[2] = 2;
+    for (int i = 3; i <= n; i++) {
+      dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
   }
 
   public int longestCommonSubsequence(String text1, String text2) {
