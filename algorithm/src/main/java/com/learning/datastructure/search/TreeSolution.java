b368d12666b8658443f68a394adc55431bf91523
tingfa.yang <tingfa.yang@duitang.com>
2024-04-07 17:57:33 +0800

    add island

--- algorithm/src/main/java/com/learning/datastructure/search/TreeSolution.java
+++ algorithm/src/main/java/com/learning/datastructure/search/TreeSolution.java
@@ -9,6 +9,8 @@ public class TreeSolution {
 
   public static void main(String[] args) {
     TreeSolution treeSolution = new TreeSolution();
+    treeSolution.islandPerimeter();
+    treeSolution.numIslands();
     treeSolution.solve();
     treeSolution.hasPathSum();
   }
@@ -30,6 +32,124 @@ public class TreeSolution {
     };
     solve(board);
     System.out.println(board);
+  }
+
+  public void numIslands() {
+    char[][] grid = {
+        {'1', '1', '1', '1', '0'},
+        {'1', '1', '0', '1', '0'},
+        {'1', '1', '0', '0', '0'},
+        {'0', '0', '0', '0', '0'}
+    };
+    int i = numIslands(grid);
+    System.out.println(i);
+  }
+
+  private void islandPerimeter() {
+    int[][] grid = {
+        {0, 1, 0, 0},
+        {1, 1, 1, 0},
+        {0, 1, 0, 0},
+        {1, 1, 0, 0}
+    };
+    int i = islandPerimeter(grid);
+    System.out.println(i);
+  }
+
+  public int islandPerimeter(int[][] grid) {
+    int result = 0;
+    for (int i = 0; i < grid.length; i++) {
+      for (int j = 0; j < grid[i].length; j++) {
+        if (grid[i][j] == 1) {
+          result = dfsIslandPerimeter(grid, i, j);
+        }
+      }
+    }
+    return result;
+  }
+
+
+  private int dfsIslandPerimeter(int[][] grid, int i, int j) {
+    if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length) {
+      return 1;
+    }
+    if (grid[i][j] == 0) {
+      return 1;
+    }
+    if (grid[i][j] != 1) {
+      return 0;
+    }
+    grid[i][j] = 2;
+    return dfsIslandPerimeter(grid, i - 1, j) +
+        dfsIslandPerimeter(grid, i + 1, j) +
+        dfsIslandPerimeter(grid, i, j - 1) +
+        dfsIslandPerimeter(grid, i, j + 1);
+  }
+
+
+  /**
+   * leetcode 695
+   */
+  public int maxAreaOfIsland(int[][] grid) {
+    int result = 0;
+    for (int i = 0; i < grid.length; i++) {
+      for (int j = 0; j < grid[i].length; j++) {
+        if (grid[i][j] == '1') {
+          int i1 = dfsMaxAreaOfIsland(grid, i, j);
+          if (i1 > result) {
+            result = i1;
+          }
+        }
+      }
+    }
+    return result;
+  }
+
+  private int dfsMaxAreaOfIsland(int[][] grid, int i, int j) {
+    if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length) {
+      return 0;
+    }
+    if (grid[i][j] != 1) {
+      return 0;
+    }
+    grid[i][j] = 2;
+    return 1 +
+        dfsMaxAreaOfIsland(grid, i - 1, j) +
+        dfsMaxAreaOfIsland(grid, i + 1, j) +
+        dfsMaxAreaOfIsland(grid, i, j - 1) +
+        dfsMaxAreaOfIsland(grid, i, j + 1);
+  }
+
+  /**
+   * leetcode 200
+   */
+  public int numIslands(char[][] grid) {
+    int result = 0;
+    for (int i = 0; i < grid.length; i++) {
+      for (int j = 0; j < grid[i].length; j++) {
+        if (grid[i][j] == '1') {
+          dfsIslands(grid, i, j);
+          result += 1;
+        }
+      }
+    }
+    return result;
+  }
+
+
+  private int dfsIslands(char[][] grid, int i, int j) {
+    if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length) {
+      return 0;
+    }
+    if (grid[i][j] != '1') {
+      return 0;
+    }
+    grid[i][j] = '2';
+    return 1 +
+        dfsIslands(grid, i - 1, j) +
+        dfsIslands(grid, i + 1, j) +
+        dfsIslands(grid, i, j - 1) +
+        dfsIslands(grid, i, j + 1);
   }
 
 
