743c268d6c05c3e74a90586f618f79143fedb3b8
tingfa.yang <tingfa.yang@duitang.com>
2024-04-25 11:34:52 +0800

    add alg

--- algorithm/src/main/java/com/learning/datastructure/LengthOfLIS.java
+++ algorithm/src/main/java/com/learning/datastructure/LengthOfLIS.java
@@ -15,6 +15,27 @@ public class LengthOfLIS {
 
     int[] array = {10, 9, 2, 5, 3, 7, 101, 18};
     System.out.println(lis.lengthOfLIS(array));
+    System.out.println(lis.lengthOfLIS(array, 2));
+  }
+
+
+  public int lengthOfLIS(int[] nums, int k) {
+    int[] dp = new int[nums.length];
+    Arrays.fill(dp, 1);
+    int result = 1;
+    for (int i = 0; i < nums.length; i++) {
+
+      for (int j = 0; j < i; j++) {
+        if (nums[i] > nums[j] && nums[i] - nums[j] <= k) {
+          dp[i] = Math.max(dp[i], dp[j] + 1);
+        }
+      }
+      if (dp[i] > result) {
+        result = dp[i];
+      }
+
+    }
+    return result;
   }
 
 
